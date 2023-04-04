4415c1f02522a934425eb4bd94da3a2659071b71
VNair12101992 <vishnunair121992@gmail.com>
2023-04-04 14:35:16 +0530

    Committing

---
+++ src/test/java/TestActions.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestActions {
+
+    @Test
+    public void checkTest(){
+        String a="a";
+        String b="a";
+        Assert.assertEquals(a,b);
+    }
+}
