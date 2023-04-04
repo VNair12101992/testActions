b4eba71bdd5fd492a7df6f037b11d29e2e0ff30a
VNair12101992 <vishnunair121992@gmail.com>
2023-04-04 23:10:32 +0530

    Committing

--- src/test/java/TestActions.java
+++ src/test/java/TestActions.java
@@ -14,6 +14,7 @@ public class TestActions {
 
     @Test
     public void checkTest(){
+
         System.setProperty("webdriver.http.factory", "jdk-http-client");
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
