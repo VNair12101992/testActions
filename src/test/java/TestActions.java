805629decd108b4ed610b00ea005748ae1b5d0e0
VNair12101992 <vishnunair121992@gmail.com>
2023-04-04 23:07:49 +0530

    Committing

--- src/test/java/TestActions.java
+++ src/test/java/TestActions.java
@@ -1,26 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class TestActions {
 
     @Test
     public void checkTest(){
-        String a="a";
-        String b="a";
-        Assert.assertEquals(a,b);
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.get("https://www.google.com/");
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("//input[@value='Google Search']")));
+        System.out.println(driver.getTitle());
+
     }
 
-    @Test
-    public void checkTest1(){
-        String a="a";
-        String b="a";
-        Assert.assertEquals(a,b);
-    }
-
-    @Test
-    public void checkTest2(){
-        String a="a";
-        String b="a";
-        Assert.assertEquals(a,b);
-    }
 }
