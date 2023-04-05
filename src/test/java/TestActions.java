50584130656c381c55055b143e4600fbde7b2ef3
VNair12101992 <vishnunair121992@gmail.com>
2023-04-05 23:31:21 +0530

    Committing

--- src/test/java/TestActions.java
+++ src/test/java/TestActions.java
@@ -1,11 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
